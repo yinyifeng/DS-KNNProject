f67578733380cdc530922ae69ad2e7c9291fd13e
Yinyi <33323282+yinyifeng@users.noreply.github.com>
2023-10-09 21:47:18 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import datasets
+from sklearn.decomposition import PCA
+
+%matplotlib inline
