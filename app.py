800ee160352df81fab311b5d9b43bd86a1e629bc
Yinyi <33323282+yinyifeng@users.noreply.github.com>
2023-10-10 14:13:20 +0200

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn import datasets
-from sklearn.decomposition import PCA
-
-%matplotlib inline
